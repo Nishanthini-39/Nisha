f9bbce738eddbdeeb49cfc0548df494bbf8dbc9b
Nishanthini-39 <143408482+Nishanthini-39@users.noreply.github.com>
2023-09-06 19:39:40 +0530

    C.c

---
+++ C.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int i,j,rows,space;
+printf("enter no of rows:");
+scanf("%d",&rows);
+for(i=rows;i>=1;i--)
+{
+for(space=1;space<=rows-i;space++)
+{
+printf(" ");
+}
+for(j=1;j<=2*i-1;j++)
+{
+printf("*");
+}
+printf("\n");
+}
+}
